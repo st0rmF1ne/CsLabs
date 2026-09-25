52439e25c7bb2d2ec84cefeda1d8b248286f2a5a
st0rm <st0rm.f1nee@gmail.com>
2026-09-25 21:26:31 +0500

    vv

--- sem-1-lab-02-server-config/config/Program.cs
+++ sem-1-lab-02-server-config/config/Program.cs
@@ -1,1 +1,49 @@
-﻿
+﻿using System.Runtime.Intrinsics.Arm;
+
+namespace lab02;
+
+
+public class Program
+{
+    public static void Main()
+    {
+        Console.WriteLine("Запуск сервера");
+        Console.WriteLine("Доступное кол-во оперативной памяти: ");
+        int ram = int.Parse(Console.ReadLine());
+        Console.WriteLine("Кол-во игроков: ");
+        int Players = int.Parse(Console.ReadLine());
+        bool Public = true;
+        Console.WriteLine("Установить пароль?(Y/N) ");
+        bool hasPassword = Console.ReadLine() == "Y";
+
+        Console.WriteLine(CheckConfiguration(Players, ram, Public, hasPassword));
+
+    }
+    public static string CheckConfiguration(int Players, int ram, bool Public, bool hasPassword)
+    {
+        if (Players <= 0)
+        {
+            return "Запуск невозможен: количество игроков должно быть больше нуля.";
+        }
+
+        if (ram < 2)
+        {
+            return "Запуск невозможен: серверу недостаточно оперативной памяти.";
+        }
+
+        if (Public && hasPassword)
+        {
+            return "Запуск возможен с предупреждением: публичный сервер защищён паролем.";
+        }
+
+        if (Players > ram * 25)
+        {
+            return "Запуск возможен с предупреждением: для такого количества игроков рекомендуется больше оперативной памяти.";
+        }
+
+        return "Сервер готов к запуску.";
+    }
+}
+    
+    
+    
--- sem-1-lab-02-server-config/test/UnitTest1.cs
+++ sem-1-lab-02-server-config/test/UnitTest1.cs
@@ -1,16 +1,64 @@
-﻿namespace test
-{
-    public class Tests
-    {
-        [SetUp]
-        public void Setup()
-        {
-        }
+﻿using Microsoft.VisualStudio.TestPlatform.TestHost;
+using NUnit.Framework;
 
-        [Test]
-        public void Test1()
-        {
-            Assert.Pass();
-        }
+public class ProgramTests
+{
+    [Test]
+    public void CheckConfiguration_ValidConfiguration_ServerIsReady()
+    {
+        var result = Program.CheckConfiguration(50, 8, true, false);
+
+        Assert.That(result, Is.EqualTo("Сервер готов к запуску."));
+    }
+
+    [Test]
+    public void CheckConfiguration_ZeroPlayers_LaunchIsImpossible()
+    {
+        var result = Program.CheckConfiguration(0, 8, true, false);
+
+        Assert.That(
+            result,
+            Is.EqualTo("Запуск невозможен: количество игроков должно быть больше нуля."));
+    }
+
+    [Test]
+    public void CheckConfiguration_NotEnoughMemory_LaunchIsImpossible()
+    {
+        var result = Program.CheckConfiguration(50, 1, true, false);
+
+        Assert.That(
+            result,
+            Is.EqualTo("Запуск невозможен: серверу недостаточно оперативной памяти."));
+    }
+
+    [Test]
+    public void CheckConfiguration_PublicServerWithPassword_LaunchWithWarning()
+    {
+        var result = Program.CheckConfiguration(50, 8, true, true);
+
+        Assert.That(
+            result,
+            Is.EqualTo("Запуск возможен с предупреждением: публичный сервер защищён паролем."));
+    }
+
+    [Test]
+    public void CheckConfiguration_TooManyPlayersForAvailableMemory_LaunchWithWarning()
+    {
+        var result = Program.CheckConfiguration(150, 4, true, false);
+
+        Assert.That(
+            result,
+            Is.EqualTo(
+                "Запуск возможен с предупреждением: для такого количества игроков рекомендуется больше оперативной памяти."));
+    }
+
+    [Test]
+    public void CheckConfiguration_InvalidPlayersAndPublicServerWithPassword_LaunchIsImpossible()
+    {
+        var result = Program.CheckConfiguration(0, 8, true, true);
+
+        Assert.That(
+            result,
+            Is.EqualTo("Запуск невозможен: количество игроков должно быть больше нуля."));
     }
 }
